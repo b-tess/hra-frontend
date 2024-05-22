6813d1eec542a7fc3a03021f52e9a892d0c9928a
Barbara Tess <barbaratessn@gmail.com>
2024-05-22 08:04:42 +0300

    Remove unnecessary Vite code
    Update the NavBar code as some elements from
    Headless UI, that are in the code snippet from Tailwind, have been
    deprecated.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,14 @@
 import { Fragment } from 'react'
-import { Disclosure, Menu, Transition } from '@headlessui/react'
+import {
+    Disclosure,
+    DisclosureButton,
+    DisclosurePanel,
+    Menu,
+    MenuButton,
+    MenuItems,
+    MenuItem,
+    Transition,
+} from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 
 const navigation = [
@@ -13,7 +22,7 @@ function classNames(...classes) {
     return classes.filter(Boolean).join(' ')
 }
 
-export default function Example() {
+export default function NavBar() {
     return (
         <Disclosure
             as='nav'
@@ -25,7 +34,7 @@ export default function Example() {
                         <div className='relative flex h-16 items-center justify-between'>
                             <div className='absolute inset-y-0 left-0 flex items-center sm:hidden'>
                                 {/* Mobile menu button*/}
-                                <Disclosure.Button className='relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white'>
+                                <DisclosureButton className='relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white'>
                                     <span className='absolute -inset-0.5' />
                                     <span className='sr-only'>
                                         Open main menu
@@ -41,7 +50,7 @@ export default function Example() {
                                             aria-hidden='true'
                                         />
                                     )}
-                                </Disclosure.Button>
+                                </DisclosureButton>
                             </div>
                             <div className='flex flex-1 items-center justify-center sm:items-stretch sm:justify-start'>
                                 <div className='flex flex-shrink-0 items-center'>
@@ -96,7 +105,7 @@ export default function Example() {
                                     className='relative ml-3'
                                 >
                                     <div>
-                                        <Menu.Button className='relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800'>
+                                        <MenuButton className='relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800'>
                                             <span className='absolute -inset-1.5' />
                                             <span className='sr-only'>
                                                 Open user menu
@@ -106,7 +115,7 @@ export default function Example() {
                                                 src='https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
                                                 alt=''
                                             />
-                                        </Menu.Button>
+                                        </MenuButton>
                                     </div>
                                     <Transition
                                         as={Fragment}
@@ -117,63 +126,27 @@ export default function Example() {
                                         leaveFrom='transform opacity-100 scale-100'
                                         leaveTo='transform opacity-0 scale-95'
                                     >
-                                        <Menu.Items className='absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'>
-                                            <Menu.Item>
-                                                {({ active }) => (
-                                                    <a
-                                                        href='#'
-                                                        className={classNames(
-                                                            active
-                                                                ? 'bg-gray-100'
-                                                                : '',
-                                                            'block px-4 py-2 text-sm text-gray-700'
-                                                        )}
-                                                    >
-                                                        Your Profile
-                                                    </a>
-                                                )}
-                                            </Menu.Item>
-                                            <Menu.Item>
-                                                {({ active }) => (
-                                                    <a
-                                                        href='#'
-                                                        className={classNames(
-                                                            active
-                                                                ? 'bg-gray-100'
-                                                                : '',
-                                                            'block px-4 py-2 text-sm text-gray-700'
-                                                        )}
-                                                    >
-                                                        Settings
-                                                    </a>
-                                                )}
-                                            </Menu.Item>
-                                            <Menu.Item>
-                                                {({ active }) => (
-                                                    <a
-                                                        href='#'
-                                                        className={classNames(
-                                                            active
-                                                                ? 'bg-gray-100'
-                                                                : '',
-                                                            'block px-4 py-2 text-sm text-gray-700'
-                                                        )}
-                                                    >
-                                                        Sign out
-                                                    </a>
-                                                )}
-                                            </Menu.Item>
-                                        </Menu.Items>
+                                        <MenuItems className='absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'>
+                                            <MenuItem className='data-[focus]:bg-gray-100 block px-4 py-2 text-sm text-gray-700'>
+                                                <a href='#'>Your Profile</a>
+                                            </MenuItem>
+                                            <MenuItem className='data-[focus]:bg-gray-100 block px-4 py-2 text-sm text-gray-700'>
+                                                <a href='#'>Your Profile</a>
+                                            </MenuItem>
+                                            <MenuItem className='data-[focus]:bg-gray-100 block px-4 py-2 text-sm text-gray-700'>
+                                                <a href='#'>Your Profile</a>
+                                            </MenuItem>
+                                        </MenuItems>
                                     </Transition>
                                 </Menu>
                             </div>
                         </div>
                     </div>
 
-                    <Disclosure.Panel className='sm:hidden'>
+                    <DisclosurePanel className='sm:hidden'>
                         <div className='space-y-1 px-2 pb-3 pt-2'>
                             {navigation.map((item) => (
-                                <Disclosure.Button
+                                <DisclosureButton
                                     key={item.name}
                                     as='a'
                                     href={item.href}
@@ -188,10 +161,10 @@ export default function Example() {
                                     }
                                 >
                                     {item.name}
-                                </Disclosure.Button>
+                                </DisclosureButton>
                             ))}
                         </div>
-                    </Disclosure.Panel>
+                    </DisclosurePanel>
                 </>
             )}
         </Disclosure>
