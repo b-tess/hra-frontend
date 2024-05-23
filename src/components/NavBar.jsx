fb5021d602a3cceaa76d35f3809303a3eb2051ae
Barbara Tess <barbaratessn@gmail.com>
2024-05-23 04:16:49 +0300

    Dynamic styling of header links

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,4 @@
-import { Fragment } from 'react'
+import { Fragment, useState, useEffect } from 'react'
 import {
     Disclosure,
     DisclosureButton,
@@ -11,22 +11,41 @@ import {
 } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 
-const navigation = [
-    { name: 'Dashboard', href: '#', current: true },
-    { name: 'Team', href: '#', current: false },
-    { name: 'Projects', href: '#', current: false },
-    { name: 'Calendar', href: '#', current: false },
-]
-
 function classNames(...classes) {
     return classes.filter(Boolean).join(' ')
 }
 
 export default function NavBar() {
+    // const [currentState, setCurrentState] = useState(false)
+    const [isActive, setIsActive] = useState(false)
+    // const [navigation, setNavigation] = useState([
+    //     { name: 'Dashboard', href: '#', current: false },
+    //     { name: 'Team', href: '#', current: false },
+    //     { name: 'Projects', href: '#', current: false },
+    //     { name: 'Calendar', href: '#', current: false },
+    // ])
+
+    const navigation = [
+        { name: 'Dashboard', href: '#' },
+        { name: 'Team', href: '#' },
+        { name: 'Projects', href: '#' },
+        { name: 'Calendar', href: '#' },
+    ]
+
+    useEffect(() => {
+        setIsActive('Dashboard')
+    }, [])
+
+    // function changeLinkStyle(index) {
+    //     const newNav = [...navigation]
+    //     newNav[index].current = true
+
+    //     setNavigation(newNav)
+    // }
     return (
         <Disclosure
             as='nav'
-            className='bg-gray-800'
+            className='bg-white'
         >
             {({ open }) => (
                 <>
@@ -55,27 +74,31 @@ export default function NavBar() {
                             <div className='flex flex-1 items-center justify-center sm:items-stretch sm:justify-start'>
                                 <div className='flex flex-shrink-0 items-center'>
                                     <img
-                                        className='h-8 w-auto'
-                                        src='https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500'
-                                        alt='Your Company'
+                                        className='w-16 h-auto'
+                                        src='./hra-logo.jpg'
+                                        alt='Health Risk Assessment Logo'
                                     />
                                 </div>
                                 <div className='hidden sm:ml-6 sm:block'>
                                     <div className='flex space-x-4'>
-                                        {navigation.map((item) => (
+                                        {navigation.map((item, index) => (
                                             <a
                                                 key={item.name}
                                                 href={item.href}
                                                 className={classNames(
-                                                    item.current
+                                                    isActive === item.name
                                                         ? 'bg-gray-900 text-white'
                                                         : 'text-gray-300 hover:bg-gray-700 hover:text-white',
                                                     'rounded-md px-3 py-2 text-sm font-medium'
                                                 )}
                                                 aria-current={
-                                                    item.current
+                                                    isActive
                                                         ? 'page'
                                                         : undefined
+                                                }
+                                                onClick={() =>
+                                                    // changeLinkStyle(index)
+                                                    setIsActive(item.name)
                                                 }
                                             >
                                                 {item.name}
@@ -84,61 +107,13 @@ export default function NavBar() {
                                     </div>
                                 </div>
                             </div>
-                            <div className='absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0'>
-                                <button
-                                    type='button'
-                                    className='relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800'
+                            <div className='absolute inset-y-0 right-0 flex items-center pr-1 sm:static sm:inset-auto sm:ml-6 sm:pr-0'>
+                                <a
+                                    href='#'
+                                    className='relative rounded-md bg-gray-900 text-white hover:bg-gray-700 px-3 py-2 text-xs font-normal sm:text-sm sm:font-medium'
                                 >
-                                    <span className='absolute -inset-1.5' />
-                                    <span className='sr-only'>
-                                        View notifications
-                                    </span>
-                                    <BellIcon
-                                        className='h-6 w-6'
-                                        aria-hidden='true'
-                                    />
-                                </button>
-
-                                {/* Profile dropdown */}
-                                <Menu
-                                    as='div'
-                                    className='relative ml-3'
-                                >
-                                    <div>
-                                        <MenuButton className='relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800'>
-                                            <span className='absolute -inset-1.5' />
-                                            <span className='sr-only'>
-                                                Open user menu
-                                            </span>
-                                            <img
-                                                className='h-8 w-8 rounded-full'
-                                                src='https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
-                                                alt=''
-                                            />
-                                        </MenuButton>
-                                    </div>
-                                    <Transition
-                                        as={Fragment}
-                                        enter='transition ease-out duration-100'
-                                        enterFrom='transform opacity-0 scale-95'
-                                        enterTo='transform opacity-100 scale-100'
-                                        leave='transition ease-in duration-75'
-                                        leaveFrom='transform opacity-100 scale-100'
-                                        leaveTo='transform opacity-0 scale-95'
-                                    >
-                                        <MenuItems className='absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'>
-                                            <MenuItem className='data-[focus]:bg-gray-100 block px-4 py-2 text-sm text-gray-700'>
-                                                <a href='#'>Your Profile</a>
-                                            </MenuItem>
-                                            <MenuItem className='data-[focus]:bg-gray-100 block px-4 py-2 text-sm text-gray-700'>
-                                                <a href='#'>Your Profile</a>
-                                            </MenuItem>
-                                            <MenuItem className='data-[focus]:bg-gray-100 block px-4 py-2 text-sm text-gray-700'>
-                                                <a href='#'>Your Profile</a>
-                                            </MenuItem>
-                                        </MenuItems>
-                                    </Transition>
-                                </Menu>
+                                    Book an appointment
+                                </a>
                             </div>
                         </div>
                     </div>
@@ -151,14 +126,13 @@ export default function NavBar() {
                                     as='a'
                                     href={item.href}
                                     className={classNames(
-                                        item.current
+                                        isActive === item.name
                                             ? 'bg-gray-900 text-white'
                                             : 'text-gray-300 hover:bg-gray-700 hover:text-white',
                                         'block rounded-md px-3 py-2 text-base font-medium'
                                     )}
-                                    aria-current={
-                                        item.current ? 'page' : undefined
-                                    }
+                                    aria-current={isActive ? 'page' : undefined}
+                                    onClick={() => setIsActive(item.name)}
                                 >
                                     {item.name}
                                 </DisclosureButton>
