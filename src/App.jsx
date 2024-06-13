5847a9a9a30aff5172510481108aa34b98c8db36
Barbara Tess <barbaratessn@gmail.com>
2024-06-13 16:00:16 +0300

    Addition of a call-to-action section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import NavBar from './components/NavBar'
 import HeroSection from './components/HeroSection'
 import AboutSection from './components/AboutSection'
+import AssistanceSection from './components/AssistanceSection'
 
 function App() {
     return (
@@ -8,6 +9,7 @@ function App() {
             <NavBar />
             <HeroSection />
             <AboutSection />
+            <AssistanceSection />
             <p className='text-red-800'>This is a test.</p>
         </>
     )
---
+++ src/components/AssistanceSection.jsx
@@ -0,0 +1,37 @@
+import { MdOutlineMedicalInformation } from 'react-icons/md'
+
+function AssistanceSection() {
+    return (
+        <section className='w-full flex items-center justify-center'>
+            <div className='flex flex-col gap-1 items-center justify-center w-3/5 rounded-3xl bg-bright-red py-3'>
+                <MdOutlineMedicalInformation className='text-4xl text-black' />
+                <h3 className='text-base text-center font-bold text-white'>
+                    Do You...
+                </h3>
+                <p className='text-white text-center md:text-left px-1 md:px-0'>
+                    Need clarification on medical queries?
+                </p>
+                <p className='text-white text-center md:text-left px-1 md:px-0'>
+                    Have questions about a certain condition?
+                </p>
+                <p className='text-white text-center md:text-left px-1 md:px-0'>
+                    Have lab tests that you need done?
+                </p>
+                <p className='text-white text-center md:text-left px-1 md:px-0'>
+                    Have a nursing procedure that you need taken care of?
+                </p>
+                <p className='text-white font-medium text-center md:text-left px-1 md:px-0'>
+                    We can help with all that and more...
+                </p>
+                <a
+                    href='#'
+                    className='bg-white rounded-md px-3 py-2 text-black text-center font-medium'
+                >
+                    Contact Us
+                </a>
+            </div>
+        </section>
+    )
+}
+
+export default AssistanceSection
