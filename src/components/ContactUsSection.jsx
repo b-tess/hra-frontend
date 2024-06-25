1fd084347d7bb9626ddc5b36fc817cb806c06857
Barbara Tess <barbaratessn@gmail.com>
2024-06-25 21:40:25 +0300

    Start of contact us section

---
+++ src/components/ContactUsSection.jsx
@@ -0,0 +1,17 @@
+function ContactUsSection() {
+    return (
+        <section>
+            <h2 className='my-5 text-3xl text-center'>Contact Us</h2>
+            <div className='grid grid-cols-2 grid-rows-1'>
+                <div className='flex flex-col items-center justify-center'>
+                    <h3 className='text-base text-center'>Kilimani</h3>
+                </div>
+                <div className='flex flex-col'>
+                    <h3 className='text-base text-center'>Parklands</h3>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default ContactUsSection
