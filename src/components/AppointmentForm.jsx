e6147de0ec105e729cce896340201ef6b7a91f24
Barbara Tess <barbaratessn@gmail.com>
2024-07-04 15:56:38 +0300

    Addition of a date & time picker

--- src/components/AppointmentForm.jsx
+++ src/components/AppointmentForm.jsx
@@ -1,44 +1,68 @@
+import { useState } from 'react'
+import DatePicker from './DatePicker'
+
 function AppointmentForm() {
+    const [formData, setFormData] = useState({
+        name: '',
+        nationalId: '',
+        phone: '',
+        email: '',
+    })
+
+    //Destructure the form data
+    const { name, nationalId, phone, email } = formData
+
+    function handleChange(e) {
+        setFormData((prevState) => ({
+            ...prevState,
+            [e.target.id]: e.target.value,
+        }))
+    }
     return (
         <section className='flex flex-col justify-center items-center'>
             <h2 className='my-5 text-3xl text-center'>Book An Appointment</h2>
-            <form className='w-4/5 flex flex-col gap-y-2 border rounded-lg border-black p-1'>
+            <form className='w-4/5 flex flex-col gap-y-2 border rounded-lg border-black p-3'>
                 <label htmlFor='name'>Name</label>
                 <input
-                    className='py-2 bg-inherit text-translucent-black shadow-diff focus:ring-1 focus:ring-red-500'
+                    className='py-2 bg-inherit text-translucent-black shadow-diff rounded-md focus:outline-none focus:ring-1 focus:ring-black focus:text-black'
                     type='text'
                     id='name'
                     name='name'
                     placeholder='Name'
-                    value={''}
+                    value={name}
+                    onChange={handleChange}
                 />
-                <label htmlFor='nationalid'>National ID</label>
+                <label htmlFor='nationalId'>National ID</label>
                 <input
-                    className='py-2 bg-inherit text-translucent-black shadow-diff'
+                    className='py-2 bg-inherit text-translucent-black shadow-diff rounded-md focus:outline-none focus:ring-1 focus:ring-black focus:text-black'
                     type='text'
-                    id='nationalid'
-                    name='nationalid'
+                    id='nationalId'
+                    name='nationalId'
                     placeholder='National ID'
-                    value={''}
+                    value={nationalId}
+                    onChange={handleChange}
                 />
                 <label htmlFor='phone'>Phone Number</label>
                 <input
-                    className='py-2 bg-inherit text-translucent-black shadow-diff'
+                    className='py-2 bg-inherit text-translucent-black shadow-diff rounded-md focus:outline-none focus:ring-1 focus:ring-black focus:text-black'
                     type='text'
                     id='phone'
                     name='phone'
                     placeholder='Phone Number'
-                    value={''}
+                    value={phone}
+                    onChange={handleChange}
                 />
                 <label htmlFor='email'>Email</label>
                 <input
-                    className='py-2 bg-inherit text-translucent-black shadow-diff'
+                    className='py-2 bg-inherit text-translucent-black shadow-diff rounded-md focus:outline-none focus:ring-1 focus:ring-black focus:text-black'
                     type='email'
                     id='email'
                     name='email'
                     placeholder='Email'
-                    value={''}
+                    value={email}
+                    onChange={handleChange}
                 />
+                <DatePicker />
             </form>
         </section>
     )
