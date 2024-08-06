a1579d825b2de9359c17f58463322232eac6a6f6
Barbara Tess <barbaratessn@gmail.com>
2024-08-06 16:41:57 +0300

    Improve on the form display
    Add formik and yup for form validation
    Still working on integrating form state management
    using both formik and the datepicker
    
    Configure emailjs to send an email on form submit

--- src/components/AppointmentForm.jsx
+++ src/components/AppointmentForm.jsx
@@ -6,35 +6,82 @@ import {
     setMinutes,
 } from 'react-datepicker/dist/date_utils.d.ts'
 import 'react-datepicker/dist/react-datepicker.css'
+import emailjs from '@emailjs/browser'
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
 
 function AppointmentForm() {
     const [startDate, setStartDate] = useState(
         setHours(setMinutes(new Date(), 30), 8)
     )
-    const [formData, setFormData] = useState({
-        name: '',
-        nationalId: '',
-        phone: '',
-        email: '',
-    })
+    // const [formData, setFormData] = useState({
+    //     firstName: '',
+    //     lastName: '',
+    //     nationalId: '',
+    //     location: '',
+    //     phone: '',
+    //     email: '',
+    // })
 
     //Destructure the form data
-    const { name, nationalId, phone, email } = formData
+    // const { firstName, lastName, nationalId, location, phone, email } = formData
+
+    // console.log(startDate.toString())
+    const formSchema = Yup.object().shape({
+        firstName: Yup.string().required('Required.'),
+        lastName: Yup.string().required('Required.'),
+        nationalId: Yup.string()
+            .matches(/^[0-9]+$/, 'Must only be numbers.')
+            .required('Required.'),
+        phone: Yup.string()
+            .matches(/^[0-9]+$/, 'Must only be numbers.')
+            .required('Required.'),
+        email: Yup.string()
+            .email('Must be a valid email.')
+            .required('Required.'),
+    })
+
+    const formik = useFormik({
+        initialValues: {
+            firstName: '',
+            lastName: '',
+            nationalId: '',
+            location: '',
+            phone: '',
+            email: '',
+        },
+        validationSchema: formSchema,
+        onSubmit: (values) => {
+            // console.log(values)
+            const data = {
+                ...values,
+                startDate: startDate.toString(),
+            }
+
+            // console.log(data)
+            emailjs
+                .send('service_n2x11u4', 'appointment_form', data, {
+                    publicKey: 'S-_0riTS3Vhw-OG-_',
+                })
+                .then(
+                    (response) => {
+                        console.log(
+                            'Email sent successfully.',
+                            response.status,
+                            response.text
+                        )
+                    },
+                    (error) => {
+                        console.log('Email not sent.', error)
+                    }
+                )
+        },
+    })
 
     function isWeekday(date) {
         const day = getDay(date)
         return day !== 0 && day !== 6
     }
-
-    // function saturdayTime(date) {
-    //     const day = getDay(date)
-
-    //     if (day === 6) {
-    //         return setHours(setMinutes(new Date(), 0), 14)
-    //     }
-
-    //     return setHours(setMinutes(new Date(), 0), 17)
-    // }
 
     function filterPassedTime(time) {
         const currentDate = new Date()
@@ -43,71 +90,120 @@ function AppointmentForm() {
         return currentDate.getTime() < selectedDate.getTime()
     }
 
-    function handleChange(e) {
-        setFormData((prevState) => ({
-            ...prevState,
-            [e.target.id]: e.target.value,
-        }))
-    }
+    // function handleChange(e) {
+    //     setFormData((prevState) => ({
+    //         ...prevState,
+    //         [e.target.id]: e.target.value,
+    //     }))
+    // }
 
-    function handleSubmit(e) {
-        e.preventDefault()
+    // function handleSubmit(e) {
+    //     e.preventDefault()
 
-        const data = {
-            ...formData,
-            dateSelected: startDate,
-        }
+    //     const data = {
+    //         ...formData,
+    //         startDate: startDate.toString(),
+    //     }
 
-        console.log(data)
-    }
+    //     emailjs
+    //         .send('service_n2x11u4', 'appointment_form', data, {
+    //             publicKey: 'S-_0riTS3Vhw-OG-_',
+    //         })
+    //         .then(
+    //             (response) => {
+    //                 console.log(
+    //                     'Email sent successfully.',
+    //                     response.status,
+    //                     response.text
+    //                 )
+    //             },
+    //             (error) => {
+    //                 console.log('Email not sent.', error)
+    //             }
+    //         )
+    // }
     return (
         <section className='flex flex-col justify-center items-center'>
             <h2 className='my-5 text-3xl text-center'>Book An Appointment</h2>
             <form
-                className='w-4/5 flex flex-col gap-y-2 border rounded-lg border-black p-3'
-                onSubmit={handleSubmit}
+                className='w-4/5 grid grid-cols-1 grid-rows-8 md:grid-cols-2 md:grid-rows-5 gap-2 border rounded-lg border-black p-3'
+                onSubmit={formik.handleSubmit}
             >
-                <label htmlFor='name'>Name</label>
-                <input
-                    className='py-2 bg-inherit text-translucent-black shadow-diff rounded-md focus:outline-none focus:ring-1 focus:ring-black focus:text-black'
-                    type='text'
-                    id='name'
-                    name='name'
-                    placeholder='Name'
-                    value={name}
-                    onChange={handleChange}
-                />
-                <label htmlFor='nationalId'>National ID</label>
-                <input
-                    className='py-2 bg-inherit text-translucent-black shadow-diff rounded-md focus:outline-none focus:ring-1 focus:ring-black focus:text-black'
-                    type='text'
-                    id='nationalId'
-                    name='nationalId'
-                    placeholder='National ID'
-                    value={nationalId}
-                    onChange={handleChange}
-                />
-                <label htmlFor='phone'>Phone Number</label>
-                <input
-                    className='py-2 bg-inherit text-translucent-black shadow-diff rounded-md focus:outline-none focus:ring-1 focus:ring-black focus:text-black'
-                    type='text'
-                    id='phone'
-                    name='phone'
-                    placeholder='Phone Number'
-                    value={phone}
-                    onChange={handleChange}
-                />
-                <label htmlFor='email'>Email</label>
-                <input
-                    className='py-2 bg-inherit text-translucent-black shadow-diff rounded-md focus:outline-none focus:ring-1 focus:ring-black focus:text-black'
-                    type='email'
-                    id='email'
-                    name='email'
-                    placeholder='Email'
-                    value={email}
-                    onChange={handleChange}
-                />
-                <div className='flex flex-col md:flex-row gap-2 items-start md:items-center'>
+                <div className='flex flex-col'>
+                    <label htmlFor='firstName'>First Name</label>
+                    <input
+                        className='py-2 bg-inherit text-black shadow-diff rounded-md focus:outline-none focus:ring-1 focus:ring-black'
+                        type='text'
+                        id='firstName'
+                        name='firstName'
+                        placeholder='First Name'
+                        value={formik.values.firstName}
+                        onChange={formik.handleChange}
+                    />
+                </div>
+                <div className='flex flex-col'>
+                    <label htmlFor='lastName'>Last Name</label>
+                    <input
+                        className='py-2 bg-inherit text-black shadow-diff rounded-md focus:outline-none focus:ring-1 focus:ring-black'
+                        type='text'
+                        id='lastName'
+                        name='lastName'
+                        placeholder='Last Name'
+                        value={formik.values.lastName}
+                        onChange={formik.handleChange}
+                    />
+                </div>
+                <div className='flex flex-col'>
+                    <label htmlFor='nationalId'>National ID</label>
+                    <input
+                        className='py-2 bg-inherit text-black shadow-diff rounded-md focus:outline-none focus:ring-1 focus:ring-black'
+                        type='text'
+                        id='nationalId'
+                        name='nationalId'
+                        placeholder='National ID'
+                        value={formik.values.nationalId}
+                        onChange={formik.handleChange}
+                    />
+                </div>
+                <div className='flex flex-col'>
+                    <label htmlFor='location'>Location</label>
+                    <select
+                        name='location'
+                        id='location'
+                        value={formik.values.location}
+                        onChange={formik.handleChange}
+                        className='py-2 bg-inherit text-black shadow-diff rounded-md focus:outline-none focus:ring-1 focus:ring-black'
+                    >
+                        <option value=''>--Select--</option>
+                        <option value='Parklands'>Parklands</option>
+                        <option value='Kilimani'>Kilimani</option>
+                    </select>
+                </div>
+                <div className='flex flex-col'>
+                    <label htmlFor='phone'>Phone Number</label>
+                    <input
+                        className='py-2 bg-inherit text-black shadow-diff rounded-md focus:outline-none focus:ring-1 focus:ring-black'
+                        type='text'
+                        id='phone'
+                        name='phone'
+                        placeholder='Phone Number'
+                        value={formik.values.phone}
+                        onChange={formik.handleChange}
+                    />
+                </div>
+                <div className='flex flex-col'>
+                    <label htmlFor='email'>Email</label>
+                    <input
+                        className='py-2 bg-inherit text-black shadow-diff rounded-md focus:outline-none focus:ring-1 focus:ring-black'
+                        type='email'
+                        id='email'
+                        name='email'
+                        placeholder='Email'
+                        value={formik.values.email}
+                        onChange={formik.handleChange}
+                    />
+                </div>
+                <div className='flex flex-col gap-2 items-start md:flex-row md:items-center md:col-span-2'>
                     <DatePicker
                         selected={startDate}
                         onChange={(date) => setStartDate(date)}
@@ -117,7 +213,7 @@ function AppointmentForm() {
                         maxTime={setHours(setMinutes(new Date(), 0), 17)}
                         dateFormat={'MMMM d, yyyy h:mm aa'}
                         filterTime={filterPassedTime}
-                        // renderDayContents={saturdayInfo}
+                        placeholderText='Select appointment time'
                         id='date-picker'
                         className='py-2 bg-inherit text-black shadow-diff rounded-md focus:outline-none focus:ring-1 focus:ring-black'
                     />
@@ -125,7 +221,7 @@ function AppointmentForm() {
                         *First come, first served on Saturdays.
                     </span>
                 </div>
-                <div className='flex justify-center items-center'>
+                <div className='flex justify-center items-center md:col-span-2'>
                     <button
                         type='submit'
                         className='rounded-md w-min bg-black text-white hover:bg-translucent-red hover:text-black px-2 py-1 text-sm font-normal md:w-max md:px-3 md:py-2 md:font-medium'
